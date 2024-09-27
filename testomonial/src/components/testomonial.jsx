1e257610bf13c88516dfffb0a175e0b3a9ef1a4c
Dkyadav29 <dkyadav2905@gmail.com>
2024-09-27 23:23:12 +0530

    add blockquote

---
+++ testomonial/src/components/testomonial.jsx
@@ -0,0 +1,11 @@
+const testomonial = () => {
+  return (
+    <div className="header">
+      <span className="title">
+        <h1>Our client say</h1> 
+      </span>
+    </div>
+  );
+};
+
+export default testomonial;
